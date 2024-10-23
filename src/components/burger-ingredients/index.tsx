e2ed769a34fe1cbc0b79382ce1ff88cfd1b264f1
andrey <kobzev605@gmail.com>
2024-10-23 03:41:12 +0300

    feat: add component burger-ingredients

---
+++ src/components/burger-ingredients/index.tsx
@@ -0,0 +1,48 @@
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import TypeSection from "./type-section";
+import { Ingredients } from "./types";
+import { useState } from "react";
+
+type Props = {
+  listIngredients: Array<Ingredients>;
+};
+
+function BurgerIngredients({ listIngredients }: Props) {
+  const [current, setCurrent] = useState<string>("");
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", maxWidth: "600px" }}
+      className="pt-10"
+    >
+      <h2 className="text text_type_main-large">Соберите бургер</h2>
+      <div style={{ display: "flex" }} className="mt-5">
+        <Tab value="Булки" active={current === "Булки"} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value="Соусы" active={current === "Соусы"} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab
+          value="Начинки"
+          active={current === "Начинки"}
+          onClick={setCurrent}
+        >
+          Начинки
+        </Tab>
+      </div>
+      <TypeSection listIngredients={listIngredients} title="Булки" type="bun" />
+      <TypeSection
+        listIngredients={listIngredients}
+        title="Соусы"
+        type="sauce"
+      />
+      <TypeSection
+        listIngredients={listIngredients}
+        title="Начинки"
+        type="main"
+      />
+    </div>
+  );
+}
+
+export default BurgerIngredients;
