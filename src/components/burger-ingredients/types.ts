0413d4dd7db867e8c53b02bf31407a784cc708ba
andrey <kobzev605@gmail.com>
2024-10-23 03:38:31 +0300

    feat: add type for ingredient

---
+++ src/components/burger-ingredients/types.ts
@@ -0,0 +1,11 @@
+export type Ingredients = {
+  id: string;
+  name: string;
+  image: string;
+  type: string;
+  price: number;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+};
