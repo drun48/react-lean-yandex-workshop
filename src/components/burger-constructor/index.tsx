3dcfbd6ada9470074325427152655eb57ae1a984
andrey <kobzev605@gmail.com>
2024-10-23 03:41:37 +0300

    feat: add component burger-constructor

---
+++ src/components/burger-constructor/index.tsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  ConstructorElement,
+  CurrencyIcon,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import style from "./burger-constructor.module.css";
+
+function BurgerConstructor() {
+  const data = [
+    {
+      isLocked: true,
+      text: "Краторная булка N-200i (верх)",
+      price: 200,
+      thumbnail: "https://code.s3.yandex.net/react/code/mineral_rings.png",
+    },
+    {
+      isLocked: false,
+      text: "Краторная булка N-200i (верх)",
+      price: 200,
+      thumbnail: "https://code.s3.yandex.net/react/code/mineral_rings.png",
+    },
+    {
+      isLocked: true,
+      text: "Краторная булка N-200i (верх)",
+      price: 200,
+      thumbnail: "https://code.s3.yandex.net/react/code/mineral_rings.png",
+    },
+  ];
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          height: "auto",
+          overflow: "auto",
+        }}
+        className="mt-25 mb-10"
+      >
+        {data.map((el, index) => {
+          const type =
+            index === 0
+              ? "top"
+              : index === data.length - 1
+              ? "bottom"
+              : undefined;
+          return (
+            <div style={{position:"relative"}} className="pl-8" key={index}>
+              {!el.isLocked && <DragIcon type="primary" className={style.icon_drag}/>}
+              <ConstructorElement {...el} type={type} />
+            </div>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <p className="type text_type_digits-medium">610</p>
+        <CurrencyIcon
+          type="primary"
+          className={style.icon + " " + "mr-10 ml-2"}
+        />
+        <Button htmlType="button" type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default BurgerConstructor;
