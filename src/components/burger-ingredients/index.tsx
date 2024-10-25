91659deaaaecf178a4d44101f884096cd4cd71c9
andrey <kobzev605@gmail.com>
2024-10-25 14:36:07 +0300

    fix: style for burger-ingredients

--- src/components/burger-ingredients/index.tsx
+++ src/components/burger-ingredients/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import IngredientDetails from "./ingredient-details";
 import Loader from "../loader";
 import { apiURL } from "../../constants";
+import styles from "./burger-ingredients.module.css";
 
 function BurgerIngredients() {
   const [isLoading, setLoading] = useState<boolean>(false);
@@ -21,9 +22,7 @@ function BurgerIngredients() {
     setLoading(true);
     (async () => {
       try {
-        const res = await fetch(
-          `${apiURL}/api/ingredients`
-        );
+        const res = await fetch(`${apiURL}/api/ingredients`);
         const data = (await res.json()).data;
         setLoading(false);
         setIngredients(
@@ -43,7 +42,7 @@ function BurgerIngredients() {
       } catch (e) {
         setLoading(false);
         setIngredients([]);
-        console.error(e)
+        console.error(e);
       }
     })();
   }, []);
@@ -55,24 +54,16 @@ function BurgerIngredients() {
   }, [activeIngredient]);
   return (
     <>
-      {isLoading && <Loader/>}
+      {isLoading && <Loader />}
       {isOpenModal && activeIngredient && (
         <IngredientDetails
           data={activeIngredient}
           handlerClose={setOpenModal}
         />
       )}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "80vh",
-          maxWidth: "600px",
-        }}
-        className="pt-10"
-      >
+      <div className={[styles["burger-ingredients"], "pt-10"].join(" ")}>
         <h2 className="text text_type_main-large">Соберите бургер</h2>
-        <div style={{ display: "flex" }} className="mt-5">
+        <div className={[styles["burger-ingredients__tubs"], "mt-5"].join(" ")}>
           <Tab value="Булки" active={current === "Булки"} onClick={setCurrent}>
             Булки
           </Tab>
@@ -88,8 +79,11 @@ function BurgerIngredients() {
           </Tab>
         </div>
         <div
-          style={{ display: "flex", flexDirection: "column", overflowX: "hidden" }}
-          className="mt-10 scroll"
+          className={[
+            styles["burger-ingredients__items"],
+            "mt-10",
+            "scroll",
+          ].join(" ")}
         >
           <TypeSection
             listIngredients={ingredients}
