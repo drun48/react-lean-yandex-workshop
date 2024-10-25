11cd2fc6118ae913b22da142020373bf1c76dbb0
andrey <kobzev605@gmail.com>
2024-10-25 14:20:56 +0300

    fix: style for headers

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -5,37 +5,50 @@ import {
   ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
+import styles from "./app-header.module.css";
+
 function AppHeader() {
-  const styleNavItem = {
-    display: "flex",
-    gap: "8px",
-    alignItems: "center",
-    cursor: "pointer",
-  };
   return (
-    <header
-      style={{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        width: "100%",
-        background: "var(--color-main)",
-      }}
-      className="p-4"
-    >
-      <div style={{alignItems:'center', justifyContent:'space-between'}} className="container" >
-        <nav style={{ display: "flex" }}>
-          <div style={styleNavItem} className="pr-5 pl-5 pt-4 pb-4">
+    <header className={[styles["app-header"], "p-4"].join(" ")}>
+      <div className={[styles["app-header__item"], "container"].join(" ")}>
+        <nav className={styles.nav}>
+          <div
+            className={[
+              styles["nav__item"],
+              "pr-5",
+              "pl-5",
+              "pt-4",
+              "pb-4",
+            ].join(" ")}
+          >
             <BurgerIcon type="primary" />
             <p className="text_type_main-default">Конструктор</p>
           </div>
-          <div style={styleNavItem} className="pr-5 pl-5 pt-4 pb-4 disabled">
+          <div
+            className={[
+              styles["nav__item"],
+              "pr-5",
+              "pl-5",
+              "pt-4",
+              "pb-4",
+              "disabled",
+            ].join(" ")}
+          >
             <ListIcon type="primary" />
             <p className="text_type_main-default">Лента заказов</p>
           </div>
         </nav>
         <Logo />
-        <div style={styleNavItem} className="pr-5 pl-5 pt-4 pb-4 disabled">
+        <div
+          className={[
+            styles["nav__item"],
+            "pr-5",
+            "pl-5",
+            "pt-4",
+            "pb-4",
+            "disabled",
+          ].join(" ")}
+        >
           <ProfileIcon type="primary" />
           <p className="text_type_main-default">Личный кабинет</p>
         </div>
