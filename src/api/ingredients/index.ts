b5e0498c2d426dc5571b27f37581119db3512cdd
andrey <kobzev605@gmail.com>
2024-11-08 07:45:01 +0300

    refact: add api

---
+++ src/api/ingredients/index.ts
@@ -0,0 +1,23 @@
+import { apiURL } from "../../constants";
+import { Ingredients } from "./types";
+
+export async function getListIngredients() {
+  try {
+    const res = await fetch(`${apiURL}/api/ingredients`);
+    const data = (await res.json()).data;
+    return data.map((item: Ingredients & { _id: string }) => ({
+      id: item._id,
+      name: item.name,
+      type: item.type,
+      proteins: item.proteins,
+      fat: item.fat,
+      carbohydrates: item.carbohydrates,
+      calories: item.calories,
+      price: item.price,
+      image: item.image,
+      image_large: item.image_large,
+    }));
+  } catch (e) {
+    console.log(e);
+  }
+}
