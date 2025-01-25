6dcf37a9abb640bede77b510e00ca8a2f72e035e
andrey <kobzev605@gmail.com>
2025-01-25 19:41:01 +0300

    fix: add const selector for bun element

--- cypress/e2e/сonstructor-burger.cy.ts
+++ cypress/e2e/сonstructor-burger.cy.ts
@@ -1,6 +1,11 @@
-import {} from 'cypress'
-import {} from '../support/cypress'
-import { SELECTORS_DROP_CONTAINER_INGREDIENT, SELECTORS_INGREDIENT_1 } from '../support/selectors';
+import {} from "cypress";
+import {} from "../support/cypress";
+import {
+  SELECTORS_BUN_DOWN_NAME,
+  SELECTORS_BUN_UP_NAME,
+  SELECTORS_DROP_CONTAINER_INGREDIENT,
+  SELECTORS_INGREDIENT_1,
+} from "../support/selectors";
 
 describe("Constructor Burger", () => {
   beforeEach(() => {
@@ -9,34 +14,20 @@ describe("Constructor Burger", () => {
   });
 
   it("should include two buns on top and bottom", () => {
-    cy.dropElement(
-      SELECTORS_INGREDIENT_1,
-      SELECTORS_DROP_CONTAINER_INGREDIENT
-    );
-    cy.get(
-      "[data-testid=constructor-element-bun-up] .constructor-element__text"
-    ).should("have.text", "Булка 1 (верх)");
-    cy.get(
-        "[data-testid=constructor-element-bun-down] .constructor-element__text"
-      ).should("have.text", "Булка 1 (вниз)");
+    cy.dropElement(SELECTORS_INGREDIENT_1, SELECTORS_DROP_CONTAINER_INGREDIENT);
+    cy.get(SELECTORS_BUN_UP_NAME).should("have.text", "Булка 1 (верх)");
+    cy.get(SELECTORS_BUN_DOWN_NAME).should("have.text", "Булка 1 (вниз)");
   });
 
   it("should drag the bun and replace if it was already in the designer", () => {
-    cy.dropElement(
-      SELECTORS_INGREDIENT_1,
-      SELECTORS_DROP_CONTAINER_INGREDIENT
-    );
-    cy.get(
-      "[data-testid=constructor-element-bun-up] .constructor-element__text"
-    ).should("have.text", "Булка 1 (верх)");
+    cy.dropElement(SELECTORS_INGREDIENT_1, SELECTORS_DROP_CONTAINER_INGREDIENT);
+    cy.get(SELECTORS_BUN_UP_NAME).should("have.text", "Булка 1 (верх)");
 
     cy.dropElement(
       "[data-testid=ingredient-2]",
       SELECTORS_DROP_CONTAINER_INGREDIENT
     );
-    cy.get(
-      "[data-testid=constructor-element-bun-up] .constructor-element__text"
-    ).should("have.text", "Булка 2 (верх)");
+    cy.get(SELECTORS_BUN_UP_NAME).should("have.text", "Булка 2 (верх)");
   });
 
   it("should include duplicate ingredients", () => {
--- cypress/support/selectors.ts
+++ cypress/support/selectors.ts
@@ -1,2 +1,4 @@
 export const SELECTORS_INGREDIENT_1 = "[data-testid=ingredient-1]";
 export const SELECTORS_DROP_CONTAINER_INGREDIENT = "[data-testid=container-drop]";
+export const SELECTORS_BUN_UP_NAME = '[data-testid=constructor-element-bun-up] .constructor-element__text'
+export const SELECTORS_BUN_DOWN_NAME = '[data-testid=constructor-element-bun-down] .constructor-element__text'
